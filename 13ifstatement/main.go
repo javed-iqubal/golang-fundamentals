d84842b374aab4f865ef2f8bdf05fbbb3410e7df
javed-iqubal <jiqbal07@gmail.com>
2026-04-04 15:48:59 +0530

    control structure: if-else statement

---
+++ 13ifstatement/main.go
@@ -0,0 +1,24 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	score := 75
+
+	if score >= 90 {
+		fmt.Println("Grade A+")
+	} else if score >= 80 {
+		fmt.Println("Grade A")
+	} else if score >= 70 {
+		fmt.Println("Grade B+")
+	} else if score >= 60 {
+		fmt.Println("Grade B")
+	} else if score >= 50 {
+		fmt.Println("Grade C+")
+	} else if score >= 40 {
+		fmt.Println("Grade C")
+	} else {
+		fmt.Println("Grade D")
+	}
+}
